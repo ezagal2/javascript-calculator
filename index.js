3322ecfbaf80689596aeb518056a7b96d5b58a15
Erick Zagal <ezagal2@toromail.csudh.edu>
2024-01-02 19:16:13 -0800

    Add append to display function

---
+++ index.js
@@ -0,0 +1,14 @@
+// CLACULATOR PROGRAM
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function calculate(){
+
+}
+
+function clearDisplay(){
+
+}
